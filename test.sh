12fa92c92986021d44d2336e5d942a00dc8c84ea
Colin Nelson <colinnlsn@gmail.com>
2019-09-01 16:32:19 -0700

    fix: changed repo url for validation

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 # this is kind of an expensive check, so let's not do this twice if we
 # are running more than one validate bundlescript
-VALIDATE_REPO='https://github.com/jessfraz/dockerfiles.git'
+VALIDATE_REPO='https://github.com/orthros/dockerfiles.git'
 VALIDATE_BRANCH='master'
 
 VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
